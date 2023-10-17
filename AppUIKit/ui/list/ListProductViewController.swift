f7b3aeacfbba93cee57e7e3b027b67a6e70cc210
Lara <lara.romero@alten.es>
2023-10-17 15:25:05 +0200

    PantallaCont_update 1

--- AppUIKit/ui/list/ListProductViewController.swift
+++ AppUIKit/ui/list/ListProductViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListProductViewController.swift
 //  AppUIKit
 //
 //  Created by lara.romero on 17/10/23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListProductViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
