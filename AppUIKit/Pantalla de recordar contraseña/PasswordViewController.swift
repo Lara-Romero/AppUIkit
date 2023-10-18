00e74171f8c28f47eabb4c2b2627b82ce5a5ffaa
Lara <lara.romero@alten.es>
2023-10-18 14:45:23 +0200

    PantallaPasw_Constrains

--- AppUIKit/Pantalla de recordar contraseña/PasswordViewController.swift
+++ AppUIKit/Pantalla de recordar contraseña/PasswordViewController.swift
@@ -4,37 +4,59 @@ import UIKit
 class PasswordViewController: UIViewController {
 
     @IBOutlet weak var btnContinue: UIButton!
-    
+    @IBOutlet weak var lb1: UILabel!
+    @IBOutlet weak var lb2: UILabel!
+    @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var viewContent: UIView!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         loadButton()
         loadViewP()
+        loadLb1()
+        loadLb2()
+        loadTfEmail()
+        
+    }
+    
+    func loadButton() {
+        btnContinue.setTitle("Continuar", for: .normal)
+        btnContinue.setTitleColor(.blue, for: .normal)
+        btnContinue.backgroundColor = UIColor.white
+        btnContinue.layer.cornerRadius = 15
+        btnContinue.layer.borderWidth = 2
+        btnContinue.layer.borderColor = UIColor.blue.cgColor
+        btnContinue.layer.masksToBounds = true
+
+    }
+    
+    func loadLb1() {
+        lb1.text = "¿No recuerdas tu contraseña?"
+    }
+    
+    func loadLb2() {
+        lb1.text = "Introduce tu email y te enviaremos un enlace de restablecimiento"
+    }
+    
+    func loadTfEmail() {
         
     }
 
-    
 }
 
-//Configuración de btn continue
-extension PasswordViewController {
-    func loadButton() {
-        btnContinue.frame( forAlignmentRect: CGRect(x: 100, y: 100, width: 140, height: 52))
-        btnContinue.backgroundColor = UIColor.white
-    }
-}
+
 
 //Para darle el gradiente al color de fondo
 extension PasswordViewController {
     func loadViewP() {
         
-        //Colores
+       // Colores
         let color1 = UIColor(named: "#001729")
         let color2 = UIColor(named: "#18629B")
         let gradientLayer = CAGradientLayer()
-                gradientLayer.frame = viewContent.bounds
+        gradientLayer.frame = viewContent.bounds
         
         gradientLayer.colors = [color1, color2]
         
@@ -43,4 +65,4 @@ extension PasswordViewController {
     
 }
 
-
+  
